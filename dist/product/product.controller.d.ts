f0436b3dcd50546fdaaef009233aab17ee77ff56
little-fox28 <tranthjnh288@gmail.com>
2024-04-10 16:53:45 +0700

    Update

--- dist/product/product.controller.d.ts
+++
@@ -1,8 +0,0 @@
-import { ProductService } from "./product.service";
-import { Product, ProductDocument } from "src/schema/product.schema";
-export declare class ProductController {
-    private readonly productService;
-    constructor(productService: ProductService);
-    createProduct(name: string, price: number, description?: string): Promise<ProductDocument>;
-    findAll(): Promise<Product[]>;
-}
